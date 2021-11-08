7845ed378898cf5bb581e57dfa49a6964cf23311
timi992000 <77628993+timi992000@users.noreply.github.com>
2021-11-08 15:45:34 +0100

    Numeric Box

---
+++ SportLeagueOverview/SportLeagueOverview/Core/Controls/NumericBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SportLeagueOverview.Core.Controls
+{
+  public class NumericBox : TextBox
+  {
+    public NumericBox()
+    {
+      HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+      Width = 40;
+      TextChanged += (sender, e) =>
+      {
+        if (sender is TextBox tmpTextBox)
+        {
+          if (!int.TryParse(tmpTextBox.Text, out int _))
+            BorderBrush = Brushes.Red;
+          else
+            tmpTextBox.BorderBrush = (Brush)new BrushConverter().ConvertFromString("#FFABADB3");
+        }
+
+      };
+    }
+
+  }
+}
